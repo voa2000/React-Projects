a828741fa1d3cba057fec92cb0fcfac176da2ab1
voa2000 <voa2000@hotmail.com>
2019-03-08 22:57:00 +0000

    Boiler Plate created

---
+++ components/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <div>My Component React Project</div>
+} ;
+ReactDOM.render(<App />, document.querySelector('#root'))
