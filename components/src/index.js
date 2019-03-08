ca0279a2c22e1caeaf19e79d8b79b0b87f65ac81
voa2000 <voa2000@hotmail.com>
2019-03-08 23:33:04 +0000

    Added a new component

--- components/src/index.js
+++ components/src/index.js
@@ -1,7 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 const App = () => {
-    return <div>My Component React Project</div>
-} ;
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="Avatar" src={faker.image.avatar()} />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="Avatar" src={faker.image.avatar()} />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Peter
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 4:06AM</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="Avatar" src={faker.image.avatar()} />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Mary
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 9:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
+        </div>
+    )
+};
 ReactDOM.render(<App />, document.querySelector('#root'))
