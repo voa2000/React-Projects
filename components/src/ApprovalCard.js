db6b1db6b371b99c5a87e10e5491a53943b7adac
voa2000 <voa2000@hotmail.com>
2019-03-09 07:55:00 +0000

    Added ApprovalCard component

---
+++ components/src/ApprovalCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+const ApprovalCard = () => {
+    return (
+        <div>
+            <form>
+                <button>
+                    Approve
+               </button>
+                <button>
+                    Reject
+                   </button>
+            </form>
+        </div>
+    );
+
+};
+export default ApprovalCard;
--- components/src/index.js
+++ components/src/index.js
@@ -6,8 +6,8 @@ import faker from 'faker';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Viv" timeago="Today at 12:45AM" blog="Nice dress" pic={faker.image.avatar()} />
-            <CommentDetail author="Sam" timeago="Yesterday at 12:00AM" blog="Great picture" pic={faker.image.avatar()} />
+            <CommentDetail author={faker.name.firstName()} timeago="Yesterday at 12:00AM" blog="Nice dress" pic={faker.image.avatar()} />
+            <CommentDetail author="Sam" timeago="Yesterday at 12:00AM" blog="Great picture" pic={faker.image.people()} />
             <CommentDetail author="Ben" timeago="Today at 7:15PM" blog="Nice suit" pic={faker.image.avatar()} />
         </div>
     )
