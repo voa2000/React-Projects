64ddb0cd22173193a1517e46f4c0434a022c209b
voa2000 <voa2000@hotmail.com>
2019-03-09 10:10:54 +0000

    components

--- components/src/index.js
+++ components/src/index.js
@@ -8,23 +8,38 @@ const App = () => {
     return (
         <div className="ui container comments">
             <ApprovalCard>
-            <CommentDetail>
-                author={faker.name.firstName()}
-                timeago="Yesterday at 12:00AM"
-                blog="Nice dress"
-                pic={faker.image.avatar()} >
-                </CommentDetail>
-                </ApprovalCard>
-            <CommentDetail
-                author={faker.name.firstName()}
-                timeago="Yesterday at 12:00AM"
-                blog="Great picture"
-                pic={faker.image.people()} />
-            <CommentDetail
-                author={faker.name.firstName()}
-                timeago="Today at 7:15PM"
-                blog="Nice suit"
-                pic={faker.image.avatar()} />
+            <div>
+                <h4>Warning</h4>
+            
+            Are you sure you want to add this?
+            </div>
+            </ApprovalCard>
+            <ApprovalCard>
+            <div>
+                <h4>Warning</h4>
+            
+            Are you sure you want to add this?
+            </div>
+                <CommentDetail
+                    author={faker.name.firstName()}
+                    timeago="Yesterday at 12:00AM"
+                    blog="Nice dress"
+                    pic={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author={faker.name.firstName()}
+                    timeago="Yesterday at 12:00AM"
+                    blog="Great picture"
+                    pic={faker.image.people()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author={faker.name.firstName()}
+                    timeago="Today at 7:15PM"
+                    blog="Nice suit"
+                    pic={faker.image.avatar()} />
+            </ApprovalCard>
         </div>
     )
 };
