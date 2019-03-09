4cc09912491c6b3d09f19ce0dfdb55d47e5006b5
voa2000 <voa2000@hotmail.com>
2019-03-09 08:52:33 +0000

    Adding buttons with css fake

--- components/src/ApprovalCard.js
+++ components/src/ApprovalCard.js
@@ -1,17 +1,15 @@
 import React from 'react';
-const ApprovalCard = () => {
+const ApprovalCard = (props) => {
     return (
-        <div>
-            <form>
-                <button>
-                    Approve
-               </button>
-                <button>
-                    Reject
-                   </button>
-            </form>
+        <div className ="ui card">
+        <div className="content">Are you sure?</div>
+         <div className="extra content">
+             <div className="ui two buttons">
+                 <div className="ui basic green button">Approve</div>
+                 <div className="ui basic red button">Reject</div>
+             </div>
+         </div>  
         </div>
     );
-
 };
 export default ApprovalCard;
--- components/src/index.js
+++ components/src/index.js
@@ -2,13 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
 import faker from 'faker';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author={faker.name.firstName()} timeago="Yesterday at 12:00AM" blog="Nice dress" pic={faker.image.avatar()} />
-            <CommentDetail author="Sam" timeago="Yesterday at 12:00AM" blog="Great picture" pic={faker.image.people()} />
-            <CommentDetail author="Ben" timeago="Today at 7:15PM" blog="Nice suit" pic={faker.image.avatar()} />
+            <ApprovalCard>
+            <CommentDetail>
+                author={faker.name.firstName()}
+                timeago="Yesterday at 12:00AM"
+                blog="Nice dress"
+                pic={faker.image.avatar()} >
+                </CommentDetail>
+                </ApprovalCard>
+            <CommentDetail
+                author={faker.name.firstName()}
+                timeago="Yesterday at 12:00AM"
+                blog="Great picture"
+                pic={faker.image.people()} />
+            <CommentDetail
+                author={faker.name.firstName()}
+                timeago="Today at 7:15PM"
+                blog="Nice suit"
+                pic={faker.image.avatar()} />
         </div>
     )
 };
