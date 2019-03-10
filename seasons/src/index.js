fc5abd2ff8ffa34296118b35491db31031548645
voa2000 <voa2000@hotmail.com>
2019-03-10 21:09:48 +0000

    Conditional statements added to render

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,21 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import SeasonDisplay from './SeasonDisplay';
 
-class App extends React.Component{
-constructor(props){
-    super(props);
-this.state={lat:40};
-}
+class App extends React.Component {
+    constructor(props) {
+        super(props);
 
-    render(){
+        this.state = {
+            lat: null,
+            errorMessage: ''};
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (error) => console.log(error)
+            position => {
+                this.setState({
+                    lat: position.coords.latitude});
+            },
+            error => {
+                this.setState({ errorMessage: error.message});
+            }
         );
-        return <div> <h1>Latitude : {this.state.lat}</h1>
-        </div>;
+     }
+    render() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>
+                <h1>Error : {this.state.errorMessage}</h1>
+            </div>
+        }
+        else if (!this.state.errorMessage && this.state.lat) {
+            return <div>
+                <h1>Latitude : {this.state.lat}</h1>
+            </div>
+        }
+        else {
+            return <div>
+                <h1>Loading .......</h1>
+            </div>
+        }
     };
-    }
-ReactDOM.render (
+}
+ReactDOM.render(
     <App />, document.querySelector('#root')
 );
