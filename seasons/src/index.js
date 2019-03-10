29137116487c8229a2f1a92f669f57048b7632eb
voa2000 <voa2000@hotmail.com>
2019-03-10 20:17:00 +0000

    Classess

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 //import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component{
+constructor(props){
+    super(props);
+this.state={lat:40};
+}
+
     render(){
         window.navigator.geolocation.getCurrentPosition(
             (position) => console.log(position),
             (error) => console.log(error)
         );
-        return <div> <h1>Latitude : </h1>
+        return <div> <h1>Latitude : {this.state.lat}</h1>
         </div>;
     };
     }
