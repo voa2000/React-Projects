8e85f59722e6f676c1638adc8b03c8332189c101
voa2000 <voa2000@hotmail.com>
2019-03-11 14:38:43 +0000

    Added images

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,8 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
 
+const seasonConfig = {
+summer: {
+    text:'Lets hit the beach',
+    iconName: 'sun'
+},
+winter:{
+    text:'Burr, it is chilly',
+    iconName: 'snowflake'
+}
+}
+
+const getSeason = (lat, month) => {
+    if (month > 0 && month > 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+}
 const SeasonDisplay = (props) => {
-    return <div> <h1>This is the season Display Component</h1></div>
+    const season = getSeason(props.lat, new Date().getMonth());
+    const {text,iconName} = seasonConfig[season];
+    return (<div>
+    <i className={`${iconName} icon`} />
+        <h1>{text} </h1>
+    <i className={`${iconName} icon`} />
+
+    </div>);
 
 };
 export default SeasonDisplay;
