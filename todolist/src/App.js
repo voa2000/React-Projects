b433af5b30843d6535bfaefd99d711ccc9b21b52
voa2000 <voa2000@hotmail.com>
2019-03-26 08:00:15 +0000

    Added Delete, Edit and complete

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Todo from './Todo';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false
+    };
+  }
+
   render() {
     return (
       <div className="App">
