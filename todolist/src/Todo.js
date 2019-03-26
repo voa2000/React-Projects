bb04e53457134ee05da3ee7d00df805ba1a1f70c
voa2000 <voa2000@hotmail.com>
2019-03-26 07:29:50 +0000

    Created To do Component

---
+++ todolist/src/Todo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Todo extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This message is from Todo Component</h1>
+      </div>
+    )
+  }
+}
+
+export default Todo
